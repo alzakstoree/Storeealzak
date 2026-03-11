c5be0dad92390fbc2cc6c9190c56c1f84b8eb798
alzakstoree <alzak.storee@gmail.com>
2026-03-11 15:55:32 +0300

    Create js/data.js

---
+++ js/data.js
@@ -0,0 +1,28 @@
+const gamesData = {
+    pubg: [
+        { name: '60 UC', price: '0.99', badge: 'أساسي', icon: 'fa-coins', rating: 4.5, reviews: 128 },
+        { name: '300 UC + 25', price: '4.99', badge: 'شائع', icon: 'fa-gem', rating: 4.7, reviews: 256 },
+        { name: '600 UC + 60', price: '9.99', badge: 'الأكثر', icon: 'fa-crown', rating: 4.9, reviews: 512 },
+        { name: '1500 UC + 300', price: '24.99', badge: 'VIP', icon: 'fa-crown', rating: 4.8, reviews: 128 },
+        { name: '3000 UC + 850', price: '49.99', badge: 'VIP+', icon: 'fa-crown', rating: 4.9, reviews: 64 },
+        { name: '6000 UC + 2000', price: '99.99', badge: 'ذهبية', icon: 'fa-crown', rating: 5.0, reviews: 32 }
+    ],
+    freefire: [
+        { name: '100 جوهرة', price: '1.50', badge: 'أساسي', icon: 'fa-gem', rating: 4.4, reviews: 96 },
+        { name: '341 جوهرة', price: '3.99', badge: 'شائع', icon: 'fa-gem', rating: 4.6, reviews: 184 },
+        { name: '572 جوهرة', price: '5.99', badge: 'شائع', icon: 'fa-gem', rating: 4.7, reviews: 223 },
+        { name: '1060 جوهرة', price: '11.99', badge: 'الأكثر', icon: 'fa-crown', rating: 4.8, reviews: 445 },
+        { name: '2079 جوهرة', price: '24.99', badge: 'VIP', icon: 'fa-crown', rating: 4.8, reviews: 167 },
+        { name: '5600 دايموند + 1150', price: '49.99', badge: 'ذهبية', icon: 'fa-crown', rating: 4.9, reviews: 89 }
+    ]
+};
+
+const coupons = {
+    'WELCOME10': 10,
+    'SAVE20': 20,
+    'VIP50': 50,
+    'PUBG20': 20,
+    'FREE10': 10
+};
+
+const ADMIN_PASSWORD = '1234';
