a3cc747174f6a3f49a2a273597164aeaa5ed8204
alzakstoree <alzak.storee@gmail.com>
2026-03-12 23:27:59 +0300

    Update store-data.js

--- js/store-data.js
+++ js/store-data.js
@@ -1,4 +1,5 @@
 // ==================== بيانات المتجر (المنتجات والأقسام) ====================
+// جميع الصور التي أرسلتها تم تضمينها هنا
 
 export const defaultStoreData = {
     sections: [
@@ -6,12 +7,12 @@ export const defaultStoreData = {
             id: 'games',
             name: 'الألعاب',
             icon: 'fa-gamepad',
-            image: 'https://i.ibb.co/rRSNhBXc/Screenshot-20260312-002402.jpg',
+            image: 'https://i.ibb.co/rRSNhBXc/Screenshot-20260312-002402.jpg', // صورة غلاف الألعاب
             categories: [
                 {
                     id: 'pubg',
                     name: 'PUBG',
-                    image: 'https://i.ibb.co/ccXyWp5v/Screenshot-20260309-045801.jpg',
+                    image: 'https://i.ibb.co/ccXyWp5v/Screenshot-20260309-045801.jpg', // صورة PUBG
                     products: [
                         { name: '60 UC', price: 0.99 },
                         { name: '300 UC + 25', price: 4.99 },
@@ -24,7 +25,7 @@ export const defaultStoreData = {
                 {
                     id: 'freefire',
                     name: 'Free Fire',
-                    image: 'https://i.ibb.co/ghSdR1b/Screenshot-20260309-045833.jpg',
+                    image: 'https://i.ibb.co/ghSdR1b/Screenshot-20260309-045833.jpg', // صورة Free Fire
                     products: [
                         { name: '100 جوهرة', price: 1.50 },
                         { name: '341 جوهرة', price: 3.99 },
@@ -37,7 +38,7 @@ export const defaultStoreData = {
                 {
                     id: 'cod',
                     name: 'Call of Duty',
-                    image: 'https://i.ibb.co/qYCBCS7m/Screenshot-20260309-045944.jpg',
+                    image: 'https://i.ibb.co/qYCBCS7m/Screenshot-20260309-045944.jpg', // صورة Call of Duty
                     products: [
                         { name: '500 CP', price: 4.99 },
                         { name: '1100 CP', price: 9.99 },
@@ -52,12 +53,12 @@ export const defaultStoreData = {
             id: 'apps',
             name: 'التطبيقات',
             icon: 'fa-mobile-alt',
-            image: 'https://i.ibb.co/S45Nv2TH/Screenshot-20260312-001846.jpg',
+            image: 'https://i.ibb.co/S45Nv2TH/Screenshot-20260312-001846.jpg', // صورة غلاف التطبيقات
             categories: [
                 {
                     id: 'shahid',
                     name: 'شاهد VIP',
-                    image: 'https://i.ibb.co/zTCxDXRc/Screenshot-20260312-220100.jpg',
+                    image: 'https://i.ibb.co/zTCxDXRc/Screenshot-20260312-220100.jpg', // صورة شاهد
                     products: [
                         { name: 'شهر واحد', price: 5.99 },
                         { name: '3 شهور', price: 15.99 },
@@ -67,7 +68,7 @@ export const defaultStoreData = {
                 {
                     id: 'netflix',
                     name: 'نتفلكس',
-                    image: 'https://i.ibb.co/ZpsTsKCT/Screenshot-20260312-215939.jpg',
+                    image: 'https://i.ibb.co/ZpsTsKCT/Screenshot-20260312-215939.jpg', // صورة نتفلكس
                     products: [
                         { name: 'شهر واحد', price: 7.99 },
                         { name: '3 شهور', price: 21.99 },
@@ -80,12 +81,12 @@ export const defaultStoreData = {
             id: 'cards',
             name: 'بطاقات جوجل',
             icon: 'fa-google',
-            image: 'https://i.ibb.co/TDCzWdNY/Screenshot-20260312-002552.jpg',
+            image: 'https://i.ibb.co/TDCzWdNY/Screenshot-20260312-002552.jpg', // صورة غلاف بطاقات جوجل
             categories: [
                 {
                     id: 'google',
                     name: 'بطاقات جوجل بلاي',
-                    image: 'https://i.ibb.co/27L8d2Cv/Screenshot-20260312-220435.jpg',
+                    image: 'https://i.ibb.co/27L8d2Cv/Screenshot-20260312-220435.jpg', // صورة بطاقة جوجل
                     products: [
                         { name: 'بطاقة 10$', price: 10.00 },
                         { name: 'بطاقة 25$', price: 25.00 },
